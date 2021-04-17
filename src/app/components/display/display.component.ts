9cf789db27355b34be4a02d9bac36167bcc0d6e7
Adam-Wallenrod <psredniawa@gmail.com>
2021-04-17 20:01:41 +0200

    Make pokemon move only on game arena

--- src/app/components/display/display.component.ts
+++ src/app/components/display/display.component.ts
@@ -16,41 +16,8 @@ enum Cursors {
 })
 export class DisplayComponent implements OnInit {
 
-
-  @HostListener('document:keyup', ['$event'])
-  handleKeyboardEvent(event: KeyboardEvent) {
-    this.key = event.key;
-
-    console.log('context: ', this.context);
-
-    switch (this.key) {
-      case Cursors.LEFT:
-        console.log('move left');
-        this.movePokemonXPos(-20);
-        break;
-
-      case Cursors.RIGHT:
-        console.log('move right');
-        this.movePokemonXPos(20);
-        break;
-
-
-      case Cursors.DOWN:
-        console.log('move down');
-        this.movePokemonYPos(20);
-        break;
-
-      case Cursors.UP:
-        console.log('move up');
-        this.movePokemonYPos(-20);
-        break;
-    }
-
-  }
-
-
-  constructor() {
-  }
+  readonly canvasWidth = 600;
+  readonly canvasHeight = 600;
 
   pokedex = new pokedex();
   pokemonName: string;
@@ -61,7 +28,55 @@ export class DisplayComponent implements OnInit {
   xPos: number;
   yPos: number;
 
+  /**
+   * Looks like pokemon image size is always the same.
+   * 96px x 96px.
+   */
+  pokemonWidth = 96;
+  pokemonHeight = 96;
+
+
+  @HostListener('document:keyup', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    this.key = event.key;
+    this.movePokemon();
+  }
+
+
+  constructor() {
+  }
+
+
   ngOnInit() {
+
+  }
+
+
+  movePokemon() {
+    console.log('context: ', this.context);
+
+    switch (this.key) {
+      case Cursors.LEFT:
+        console.log('move left');
+        this.movePokemonXPos(-this.pokemonWidth);
+        break;
+
+      case Cursors.RIGHT:
+        console.log('move right');
+        this.movePokemonXPos(this.pokemonWidth);
+        break;
+
+
+      case Cursors.DOWN:
+        console.log('move down');
+        this.movePokemonYPos(this.pokemonHeight);
+        break;
+
+      case Cursors.UP:
+        console.log('move up');
+        this.movePokemonYPos(-this.pokemonHeight);
+        break;
+    }
 
   }
 
@@ -91,25 +106,54 @@ export class DisplayComponent implements OnInit {
 
   setInBasePosition(url: string, x = 0, y = 0) {
     this.pokemonSpirte = new Image();
+    console.log('poke: ', this.pokemonSpirte);
     this.pokemonSpirte.src = url;
     this.pokemonSpirte.onload = () => {
-      this.context.drawImage(this.pokemonSpirte, 0, 0);
-      this.xPos = 0;
-      this.yPos = 0;
+      this.onPokemonLoad();
     };
 
   }
 
 
+  onPokemonLoad() {
+    this.context.drawImage(this.pokemonSpirte, 0, 0);
+    this.resetPokemonPosition();
+    // this.pokemonWidth = this.pokemonSpirte.naturalWidth;
+    // this.pokemonHeight = this.pokemonSpirte.naturalHeight;
+    // console.log('this.pokemonWidth: ', this.pokemonWidth);
+    // console.log('this.pokemonHeight: ', this.pokemonHeight);
+  }
+
+  resetPokemonPosition() {
+    this.xPos = 0;
+    this.yPos = 0;
+  }
+
+
   movePokemonXPos(value: number) {
-    this.context.clearRect(0,0, 600, 600);
+    this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
     this.xPos = this.xPos + value;
+
+    if (this.xPos > this.canvasWidth - this.pokemonWidth) {
+      this.xPos = this.canvasWidth - this.pokemonWidth;
+    } else if (this.xPos < 0) {
+      this.xPos = 0;
+    }
+
+    console.log('%c xPos: ', 'color: blue', this.xPos);
     this.context.drawImage(this.pokemonSpirte, this.xPos, this.yPos);
   }
 
   movePokemonYPos(value: number) {
-    this.context.clearRect(0,0, 600, 600);
+    this.context.clearRect(0, 0, 600, 600);
     this.yPos = this.yPos + value;
+    if (this.yPos > this.canvasHeight - this.pokemonHeight) {
+      this.yPos = this.canvasHeight - this.pokemonHeight;
+    } else if (this.yPos < 0) {
+      this.yPos = 0;
+    }
+
+    console.log('%c yPos: ', 'color: blue', this.yPos);
     this.context.drawImage(this.pokemonSpirte, this.xPos, this.yPos);
   }
 
